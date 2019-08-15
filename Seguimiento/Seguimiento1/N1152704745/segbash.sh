dba34d8a7bb7210e7649f1ad2e0b7b54053e4496
Jumarulanda <35287531+Jumarulanda@users.noreply.github.com>
2019-08-15 13:12:40 -0500

    Upgrade.

--- Seguimiento/Seguimiento1/N1152704745/segbash.sh
+++ Seguimiento/Seguimiento1/N1152704745/segbash.sh
@@ -1,11 +1,10 @@
 for n in {1..7}
  do
  mkdir folder${n}/
- cd folder${n}/
  for m in {1..10}
      do
-     touch archivo${m}.txt
-     echo "arbra" > archivo${m}.txt
+     touch folder${n}/archivo${m}.txt
+     echo "arbra" > folder${n}/archivo${m}.txt
      done
- cd ..
  done
+
