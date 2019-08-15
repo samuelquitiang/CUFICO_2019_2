a26f887c4de07def4df89c1e713c4a512dcb26fa
DavidSS0397 <31875788+DavidSS0397@users.noreply.github.com>
2019-08-15 13:18:51 -0500

    Create Segbash.sh

---
+++ Seguimiento/Seguimiento1/N1017248532/Segbash.sh
@@ -0,0 +1,1 @@
+for i in {1..7}; cd Folder$i; for i in {1..10}; do touch Archivo${i}.txt; echo "rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz" >> Archivo${i}.txt; done; cd ..; done
