fa6b15310277fc15bb297d108664ba9aef034256
elime920 <53834626+elime920@users.noreply.github.com>
2019-08-15 13:38:36 -0500

    Create segbash.sh

---
+++ Seguimiento/Seguimiento1/N1036401107/segbash.sh
@@ -0,0 +1,4 @@
+for i in {1..7}; do mkdir  Folder${i}; done  # Crea los 7 directorios
+for i in {1..10}; do mkdir Archivo${i}; done # Crea los 10 archivos dentro del directorio
+cat > Archivo${i}
+Elizabeth # Es la palabra que creo en mis archivos.
