d675b71f5ff56e257d7e394c2c67271198b7b64d
Jumarulanda <35287531+Jumarulanda@users.noreply.github.com>
2019-08-29 13:31:08 -0500

    script for fibonacci

---
+++ Seguimiento/Seguimiento1/N1152704745/fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+  unsigned long long int f_i=0, f_ip1=1;
+
+  for(int i=1; i<1000; i++){
+    
+    unsigned long long int f_ip2 = f_i + f_ip1;
+
+    if (f_ip2%2 == 0){
+      std::cout << f_ip2 << std::endl;
+    }
+
+    f_i = f_ip1;
+    f_ip1 = f_ip2;
+  }
+}
