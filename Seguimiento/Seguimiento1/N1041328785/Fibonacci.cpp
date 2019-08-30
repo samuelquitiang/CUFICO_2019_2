9f62c99470c1aa7d69915d40d9cb731a225b566c
lidersamir <53834555+lidersamir@users.noreply.github.com>
2019-08-30 06:50:45 -0500

    Update Fibonacci.cpp

--- Seguimiento/Seguimiento1/N1041328785/Fibonacci.cpp
+++ Seguimiento/Seguimiento1/N1041328785/Fibonacci.cpp
@@ -12,7 +12,7 @@ int numero = 1;
 int numeroant = 0;
 int paso; //esta variable contiene la informacion del numero actual
 
-for (int i=0; i<20; i++)
+for (int i=0; i<1000; i++)
 {
     if (numero%2 == 0)
     {
